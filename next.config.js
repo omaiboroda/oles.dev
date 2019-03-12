01394f94b583952b884ad571fc2d06bb82b78344
Oles Maiboroda <oles.maiboroda@gmail.com>
2019-03-12 13:37:25 +0100

    Next add target:serverless

--- next.config.js
+++ next.config.js
@@ -5,4 +5,4 @@ const withMDX = require('@zeit/next-mdx')({
 });
 const withImages = require('next-images');
 
-module.exports = withImages(withMDX(withCSS(withTypescript())));
+module.exports = withImages(withMDX(withCSS(withTypescript({ target: 'serverless' }))));
