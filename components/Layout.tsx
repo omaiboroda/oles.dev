1c4ed6442cfb2e20d291fb5e64ce4cabd0d416d8
Oles Maiboroda <oles.maiboroda@gmail.com>
2019-03-20 08:27:19 +0100

    Add Google verification

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -37,6 +37,10 @@ class Layout extends Component {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
           <link href="https://fonts.googleapis.com/css?family=Merriweather:400" rel="stylesheet" />
+          <meta
+            name="google-site-verification"
+            content="2XU6kHQPBdzNI3_oRD7yx4-4jWGeyrLc1h73EPEi3xM"
+          />
         </Head>
         <ThemeProvider theme={theme}>
           <Background>{this.props.children}</Background>
