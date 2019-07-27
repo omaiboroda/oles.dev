1344681046a63f2743afb13405757cca36e7e602
Oles Maiboroda <oles_maiboroda@bonify.de>
2019-07-27 14:30:27 +0200

    Add styling for figcaption

--- components/Post.tsx
+++ components/Post.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import Head from 'next/head';
 import { H1 } from './Typography';
 import { Box } from './system';
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from 'react-markdown/with-html';
 import CodeBlock from './CodeBlock';
 import 'github-markdown-css';
 import styled from 'styled-components';
 
 const PostContainer = styled(Box)`
+  line-height: 2;
   font-family: Merriweather;
+  figcaption {
+    color: ${p => p.theme.colors.greyDark};
+    text-align: center;
+    font-size: ${p => p.theme.fontSizes[1]}px;
+    margin: -10px 0 10px;
+  }
+  img {
+    display: block;
+  }
 `;
 
 const Post = ({ slug }: { slug: string }) => {
@@ -20,9 +30,9 @@ const Post = ({ slug }: { slug: string }) => {
       <Head>
         <title>{metadata.default.title} - Oles Maiboroda</title>
       </Head>
-      <PostContainer pb={2} className="markdown-body">
+      <PostContainer pb={4} className="markdown-body">
         <H1 textAlign="center">{metadata.default.title}</H1>
-        <ReactMarkdown source={Post.default} renderers={{ code: CodeBlock }} />
+        <ReactMarkdown escapeHtml={false} source={Post.default} renderers={{ code: CodeBlock }} />
       </PostContainer>
     </>
   );
