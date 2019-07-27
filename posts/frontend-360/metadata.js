7e079d7e321b7d22494a59757fc5b9274c574616
Oles Maiboroda <oles_maiboroda@bonify.de>
2019-07-27 20:20:23 +0200

    Add Frontend 360 post

---
+++ posts/frontend-360/metadata.js
@@ -0,0 +1,13 @@
+import image from './image.jpeg';
+
+const metadata = {
+  description: 'Frontend 360',
+  tags: ['frontend', 'overview', 'mindmap'],
+  slug: 'frontend-360',
+  title: 'Frontend 360',
+  thumbnail: image,
+  abstract:
+    "I needed to organize my thoughts on Frontend so I've ended up with a mindmap that I've ambitiously named as Frontend 360."
+};
+
+export default metadata;
